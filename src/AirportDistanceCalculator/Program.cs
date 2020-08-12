1116398f5fad8fee0659599e6fb4f896341346d1
Igor Bespalchuk <ibespalchuk@custis.ru>
2020-08-12 17:22:03 +0300

    Blank projects

---
+++ src/AirportDistanceCalculator/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace AirportDistanceCalculator
+{
+    /// <summary>Main entry point</summary>
+    public static class Program
+    {
+        /// <summary>Main entry point</summary>
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/UnitTests/UnitTest.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace UnitTests
+{
+    /// <summary>Set of unit tests</summary>
+    [Collection(nameof(UnitTest))]
+    public class UnitTest
+    {
+        /// <summary>
+        /// Skeleton
+        /// </summary>
+        [Fact]
+        public void Test()
+        {
+        }
+    }
+}
